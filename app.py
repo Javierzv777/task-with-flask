4a8c684d7a6aa37bec5a7fc8e0fb66aa746ea333
“Javier <javierzv777@gmail.com>
2023-11-06 11:50:32 -0500

    test credential

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 app.app_context().push()
 
+
+
 from routes import *
 if __name__ == "__main__":
     app.run(debug=True)
